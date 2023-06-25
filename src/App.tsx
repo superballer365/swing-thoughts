c4820e710110b0db8d2c42e3dfd33079eba67698
superballer365 <matthew.balnis@uconn.edu>
2023-06-25 16:17:48 -0400

    Adding vercel plugin and test endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  React.useEffect(() => {
+    fetch("/api/test")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Failed to hit test endpoint", err));
+  }, []);
 
   return (
     <>
@@ -29,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
