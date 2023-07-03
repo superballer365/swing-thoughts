4c1622b90a523e1ce728079b662827baa6861880
superballer365 <matthew.balnis@uconn.edu>
2023-07-03 16:23:16 -0400

    Try imperatively navigating when not signed in

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,17 +1,20 @@
 import { useAuth } from "@clerk/clerk-react";
-import { Navigate } from "@tanstack/router";
+import { Navigate, useNavigate } from "@tanstack/router";
 import React from "react";
 
 type Props = React.PropsWithChildren<{}>;
 
 export default function ProtectedRoute({ children }: Props) {
   const { isLoaded, isSignedIn } = useAuth();
+  const navigate = useNavigate();
 
   if (!isLoaded) return null;
 
   if (!isSignedIn) {
     console.log("Redirecting to sign in page");
-    return <Navigate to="/sign-in" />;
+    navigate({ to: "/sign-in" });
+
+    return null;
   }
 
   return <>{children}</>;
